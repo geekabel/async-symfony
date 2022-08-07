65a8d83a8dcd8d6739b448ecb4fd939285da9c95
koffigodwin96 <koffigodwin96@gmail.com>
2022-08-07 02:58:49 +0200

    support php8(docker container)

--- src/MessageHandler/PurchaseConfirmationNotificationHandler.php
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -3,14 +3,15 @@
 
 namespace App\MessageHandler;
 
-use App\Message\PurchaseConfirmationNotification;
 use Mpdf\Mpdf;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+use App\Message\PurchaseConfirmationNotification;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-#[AsMessageHandler]
-class PurchaseConfirmationNotificationHandler
+
+class PurchaseConfirmationNotificationHandler implements MessageHandlerInterface
 {
     public function __construct(private MailerInterface $mailer)
     {
